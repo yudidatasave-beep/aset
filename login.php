f214ce244e40c8b9876d5207fcbb75d1aca302f4
yudidatasave-beep <yudidatasave@gmail.com>
2026-01-09 09:51:26 +0700

    update login.php
    update login

--- login.php
+++ login.php
@@ -79,7 +79,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <div class="login-container">
     <div class="login-image">
-        <img src="assets/logo3.png" alt="Logo Rumah Sakit">
+        <img src="assets/logo3.png" alt="Pemerintah Kota Cimahi">
     </div>
     <div class="login-form">
         <h3 class="mb-4">Manajemen Pemeliharaan Aset </h3>
